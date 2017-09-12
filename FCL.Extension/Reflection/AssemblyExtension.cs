3c7caf3aa4b3038ee9101b7d1bf6e71bcd98f17f
jensen <jensen.wang@perkinelmer.com>
2017-09-12 16:25:57 +0800

    add reflection folder

---
+++ FCL.Extension/Reflection/AssemblyExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCL.Reflection
+{
+    public static class AssemblyExtension
+    {
+        /// <summary>
+        /// Gets the the directory path in which current executing assembly(this extension assembly) is placed.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetWorkingDirectory()
+        {
+            return Assembly.GetExecutingAssembly().GetAssemblyDirectory();
+        }
+
+        public static string GetAssemblyDirectory(this Assembly assembly)
+        {
+            return Path.GetDirectoryName(new Uri(assembly.CodeBase).AbsolutePath);
+        }
+    }
+}
