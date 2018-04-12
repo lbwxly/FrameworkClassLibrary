d4d73af29d54c7bc48059e8aa64030f81892a248
Jensen Wang <jensen.wang@perkinelmer.com>
2018-04-12 16:14:41 +0800

    add more helper methods

---
+++ FCL.Extension/System/NullableExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCL.System
+{
+    public static class NullableExtension
+    {
+        public static string ToString(this decimal? decimalValue, string format)
+        {
+            if (decimalValue == null)
+            {
+                return string.Empty;
+            }
+
+            return decimalValue.Value.ToString(format);
+        }
+    }
+}
